263fbef94512c03ddc332d834d7b0e8c71628ff7
kimkwongu <kwongu14@gmail.com>
2021-02-03 17:44:53 +0900

    vue start

---
+++ Number.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form v-on:submit= "onSubmitForm">
+            <input type="text" ref="answer" v-model="value">
+            <button>hi!</button>
+        </form>
+        <div> 시도:{{}}</div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: "",
+            result: "",
+        }
+    },
+    methods: {
+        onSubmitForm(e) {
+            e.preventDefault();
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, "main.js"),
+  },
+  module: {
+    rules: [{}],
+  },
+  plugins: [],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+};
