5394f2fe3671cc4605b8b6592de22f799384cda9
kimkwongu <kwongu14@gmail.com>
2021-02-04 18:02:02 +0900

    숫자 야구 게임

--- Number.vue
+++ Number.vue
@@ -1,31 +1,72 @@
 <template>
-    <div>
-        <h1>{{result}}</h1>
-        <form v-on:submit= "onSubmitForm">
-            <input type="text" ref="answer" v-model="value">
-            <button>hi!</button>
-        </form>
-        <div> 시도:{{}}</div>
-    </div>
-    
+  <div>
+    <h1>{{ result }}</h1>
+    <form @submit.prevent="onSubmitForm">
+      <input minlength="4" maxlength="4" ref="answer" v-model="value" />
+      <button>input!</button>
+    </form>
+    <div>시도:{{ tries.length }}</div>
+    <ul>
+      <li v-for="t in tries">
+        <div>{{ t.try }}</div>
+        <div>{{ t.result }}</div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (var i = 0; i < 4; i += 1) {
+    const chose = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chose);
+  }
+  return array;
+};
 export default {
-    data() {
-        return {
-            value: "",
-            result: "",
+  data() {
+    return {
+      answer: getNumbers(),
+      tries: [],
+      value: "",
+      result: "",
+    };
+  },
+  methods: {
+    onSubmitForm() {
+      if (this.value === this.answer.join("")) {
+        this.tries.push({
+          try: this.value,
+          result: "홈런",
+        });
+        this.result = "홈런";
+        alert("game restart!");
+        this.tries = [];
+        this.$refs.answer.focus();
+      } else {
+        let strike = 0;
+        let ball = 0;
+        const answerArray = this.value.split("").map((v) => parseInt(v));
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === this.answer[i]) {
+            strike++;
+          } else if (this.answer.includes(answerArray[i])) {
+            ball++;
+          }
         }
+        this.tries.push({
+          try: this.value,
+          result: ` ${strike} 스트라이크, ${ball} 볼입니다.`,
+        });
+        this.value = "";
+        this.$refs.answer.focus();
+      }
     },
-    methods: {
-        onSubmitForm(e) {
-            e.preventDefault();
-        }
-    }
+  },
 };
 </script>
 
 <style>
-
 </style>
---
+++ main.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import Number from "./Number.vue";
+
+new Vue(Number).$mount("#root");
